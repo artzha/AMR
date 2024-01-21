62d3535a7e4134425b9e64fc74431fb42ba7779c
artzha <arthurz@cs.utexas.edu>
2024-01-21 11:14:39 -0600

    Added initial course starter files

---
+++ build_docker.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker build -t cs393r_starter -f Dockerfile .
+
---
+++ start_docker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+USER=$(whoami)
+
+podman run -it --net=host \
+    --gpus all \
+    -v $(pwd)/amrl_maps:/root/amrl_maps \
+    -v $(pwd)/amrl_msgs:/root/amrl_msgs \
+    -v $(pwd)/cs393r_starter:/root/cs393r_starter \
+    -v $(pwd)/ut_automata:/root/ut_automata cs393r_starter
